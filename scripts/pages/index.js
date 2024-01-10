6080789aa9fe0bf5d65d229d03c853009281a64a
Nathuz <nathuznoepupin@gmail.com>
2024-01-10 14:47:24 +0100

    🎨 Prettier

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,8 +1,8 @@
-import '../../style/pages/_index.scss'
+import "../../style/pages/_index.scss";
 
 // Sélection de la div "App"
-const app = document.querySelector('#app');
+const app = document.querySelector("#app");
 
-const mainTitle = document.createElement('h1');
+const mainTitle = document.createElement("h1");
 mainTitle.textContent = "Coucou";
 app.appendChild(mainTitle);
